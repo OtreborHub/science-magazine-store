74e0eb687d11f21d2608df51f2ef3a77e75f60f3
QuestionSigns <zito.roberto94@gmail.com>
2024-05-04 17:47:52 +0200

    changing logo

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,3 +1,4 @@
+import Typography from "@mui/material/Typography/Typography";
 import "../styles/error.css";
 import { ErrorProps } from "../utilities/interfaces";
 
@@ -5,8 +6,20 @@ export default function Error({errorMessage}: ErrorProps) {
 
 
     return (
+        <>
+        <Typography 
+            className="anta-regular" 
+            variant="h2" 
+            paddingTop={"3rem"} 
+            paddingBottom={"3rem"} 
+            textAlign={"center"}
+            color={"whitesmoke"}>
+            TECHNOLOGY INNOVATION
+        </Typography>
+
         <div className="error-div">
             {errorMessage}
         </div>
+        </>
     )
 }
